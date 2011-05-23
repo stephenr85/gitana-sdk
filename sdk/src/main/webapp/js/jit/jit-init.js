023888bda09153f1836fcf5067579ff2da20400d
drq <quyong2000@yahoo.com>
2011-05-23 11:46:21 -0400

    Added missing jit files.

---
+++ sdk/src/main/webapp/js/jit/jit-init.js
@@ -0,0 +1,23 @@
+var labelType, useGradients, nativeTextSupport, animate;
+
+(function() {
+  var ua = navigator.userAgent,
+      iStuff = ua.match(/iPhone/i) || ua.match(/iPad/i),
+      typeOfCanvas = typeof HTMLCanvasElement,
+      nativeCanvasSupport = (typeOfCanvas == 'object' || typeOfCanvas == 'function'),
+      textSupport = nativeCanvasSupport
+        && (typeof document.createElement('canvas').getContext('2d').fillText == 'function');
+  //I'm setting this based on the fact that ExCanvas provides text support for IE
+  //and that as of today iPhone/iPad current text support is lame
+  labelType = (!nativeCanvasSupport || (textSupport && !iStuff))? 'Native' : 'HTML';
+  nativeTextSupport = labelType == 'Native';
+  useGradients = nativeCanvasSupport;
+  animate = !(iStuff || !nativeCanvasSupport);
+})();
+
+var Log = {
+  write: function(text){
+    $('#log').html(text);
+    $('#log').css('left',(500 - $('#log').offsetWidth / 2) + 'px');
+  }
+};
